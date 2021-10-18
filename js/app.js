0638e4c968e89284915460d3747767fa09e33759
MarJune14 <gereromarjune14@gmail.com>
2021-10-18 10:16:36 -0700

    First Step in Project.js

--- js/app.js
+++ js/app.js
@@ -1,16 +1,33 @@
 //variables
-
+const courses = document.querySelector('#courses-list');
 
 //eventlisteners
-function eventlisteners(){
-    // from submission
-    document.querySelector('#form').addEventListener('submit', newTweet);
+
+loadEventListeners();
+
+function loadEventListeners() {
+    //adding course
+    courses.addEventListener('click', buyCourse);
+
 }
+
 
 //functions
 
-function newTweet(e) {
+function buyCourse(e) {
     e.preventDefault();
 
-    console.log('Form Submitted');
+    //use delegation to find the course added
+
+    if(e.target.classList.contains('add-to-cart')) {
+        //read the course value
+        const course = e.target.parentElement.parentElement;
+
+        //read the values
+        getCourseInfo(course);
+    }
+}
+
+function getCourseInfo(course) {
+    console.log(course);
 }
