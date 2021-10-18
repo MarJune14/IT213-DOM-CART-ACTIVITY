bebc9d8167567ffe803b65d29bce0545b033f085
MarJune14 <gereromarjune14@gmail.com>
2021-10-18 11:03:32 -0700

    Removing Courses in Shopping Cart.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 //variables
 const courses = document.querySelector('#courses-list');
+shoppingCartContent = document.querySelector('#cart-content tbody'),
+clearCartBtn = document.querySelector('#clear-cart');
 
 //eventlisteners
 
@@ -8,6 +10,10 @@ loadEventListeners();
 function loadEventListeners() {
     //adding course
     courses.addEventListener('click', buyCourse);
+    //when the remove button click
+    shoppingCartContent.addEventListener('click', removeCourse);
+     //clear cart Btn
+     clearCartBtn.addEventListener('click', clearCart);
 
 }
 
@@ -42,13 +48,40 @@ function getCourseInfo(course) {
 }
 //Display the course into the Shopping Cart
 
-function addIntoCart() {
+function addIntoCart(course) {
      //create a <tr>
      const row = document.createElement('tr');
 
      //Build the Template
      row.innerHTML = `
+        <tr>
+            <td>
+                <img src="${course.image}" width=100> 
+            </td>
+            <td>${course.title}</td>
+            <td>${course.price}</td>
+            <td>
+                <a href="#" class="remove" data-id=${course.id}">X</a>
+            </td>
+        </tr>
      
      `;
+    //add into the shopping cart
+    shoppingCartContent.appendChild(row);
+}
+//remove course from the shopping cart
+function removeCourse(e) {
+
+    if(e.target.classList.contains('remove')) {
+        e.target.parentElement.parentElement.remove();
+    }
+}
+//clear the shopping cart
+function clearCart() {
+    //shoppingCartContent.innerHTML = '';
+
+    while(shoppingCartContent.firstChild) {
+        shoppingCartContent.removeChild(shoppingCartContent.firstChild);
+    }
 
 }
